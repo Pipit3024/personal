fb8dd4907bcb81f29e720f40b78caf142fa51455
pipit3024 <pipitwulandari109@gmail.com>
2024-10-24 22:17:44 +0700

    personal

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero: React.FC = () => {
   return (
     <section
       id="home"
-      className="min-h-screen flex flex-col justify-center items-center"
+      className="min-h-screen flex flex-col justify-center items-center text-black"
       style={{ background: "linear-gradient(to bottom, #00FF00, #00FF00)" }} // Mengubah latar belakang menjadi hijau
     >
       <Image
@@ -15,10 +15,10 @@ const Hero: React.FC = () => {
         height={160}
         className="rounded slide-up"
       />
-      <h2 className="text-4xl font-bold my-4 slide-up text-black">Hello, I'M Pipit Wulandari</h2> {/* Tulisan menjadi hitam */}
-      <p className="text-6xl font-bold slide-up text-black">A CREATIVE DEVELOPER</p> {/* Tulisan menjadi hitam */}
-      <p className="slide-up text-black">Passionate about crafting elegant solutions and user-friendly designs.</p> {/* Tulisan menjadi hitam */}
-      <p className="slide-up text-black">Wanna know more about me?</p> {/* Tulisan menjadi hitam */}
+      <h2 className="text-4xl font-bold my-4 slide-up">Hello, I&apos;M Pipit Wulandari</h2> {/* Menggunakan &apos; */}
+      <p className="text-6xl font-bold slide-up">A CREATIVE DEVELOPER</p>
+      <p className="slide-up">Passionate about crafting elegant solutions and user-friendly designs.</p>
+      <p className="slide-up">Wanna know more about me?</p>
       <a href="#about" className="mt-4 text-yellow-400 slide-up">
         Click here to learn more
       </a>
